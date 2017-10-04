cb1cfc2b4728876bbb34b56f4ef7984cad5d022c
darren <764190362@qq.com>
2017-10-04 19:56:50 +0800

    继承ViewComponent基类创建ViewComponent

---
+++ ViewComponents/CitySummary.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreViewComponentsExamples.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreViewComponentsExamples.ViewComponents
+{
+    public class CitySummary:ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public string Invoke()
+        {
+            return $"{repository.Cites.Count()} cities, "
+                + $"{repository.Cites.Sum(x => x.Population)} people";
+        }
+    }
+}
