916dc727b8d48b6549900055bc078a4fdec8ad8e
darren <764190362@qq.com>
2017-10-04 14:42:54 +0800

    完成基本构建

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreViewComponentsExamples.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreViewComponentsExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository repository;
+
+        public HomeController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            repository.AddProduct(newProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ICityRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreViewComponentsExamples.Models
+{
+    public interface ICityRepository
+    {
+        IEnumerable<City> Cites { get; }
+        void AddCity(City newCity);
+    }
+}
