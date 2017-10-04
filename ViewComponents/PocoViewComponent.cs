9d8437b2d5d9a29da2d4feef93fec1a5b0bd9b50
darren <764190362@qq.com>
2017-10-04 19:52:20 +0800

    最简单的ViewComponent

---
+++ ViewComponents/PocoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreViewComponentsExamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreViewComponentsExamples.ViewComponents
+{
+    public class PocoViewComponent
+    {
+        private ICityRepository repository;
+
+        public PocoViewComponent(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public string Invoke()
+        {
+            return $"{repository.Cites.Count()} cities, "
+                + $"{repository.Cites.Sum(x => x.Population)} people";
+        }
+    }
+}
